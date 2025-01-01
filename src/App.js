2fbb180e1677077b71a4c10a1578eab1e6864c81
Cristina Ayala <crisstinaayala777@gmail.com>
2025-01-01 19:45:05 +0800

    ading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMettupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMettupPage />} />
+        <Route path="/favorites-page" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
