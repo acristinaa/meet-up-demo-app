0b114f9ad7dd724d72468a78efec46de53f479a0
Cristina Ayala <crisstinaayala777@gmail.com>
2025-01-02 13:29:43 +0800

    creating wrapping components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMettupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMettupPage />} />
         <Route path="/favorites-page" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
